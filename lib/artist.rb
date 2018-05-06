e17c6de1d3cffb78ee7d27f2d11eb364571be289
Caio Sanches <ccampato@gmail.com>
2018-05-06 07:18:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,8 +32,8 @@ class Artist
       artist
     end
 
-    def self.find_by_name(name)
-      self.all.detect { |name| self.name = name }
+    def self.find_by_name(name=)
+      self.all.detect { |name| self.name= name }
     end
 
     self.find_by_name(name) or self.create_by_name(name)
